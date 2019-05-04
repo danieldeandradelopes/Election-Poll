d7e1c1142481e10ff7a0f7a599dd246b9b04b7aa
danieldeandradelopes <danieldeandradelopes@gmali.com>
2019-05-03 23:30:07 -0300

    update file database

--- banco/banco.sql
+++ banco/banco.sql
@@ -1,23 +1,25 @@
 -- phpMyAdmin SQL Dump
--- version 4.1.14
--- http://www.phpmyadmin.net
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: 29-Set-2016 às 01:23
--- Versão do servidor: 5.6.17
--- PHP Version: 5.5.12
+-- Host: localhost
+-- Generation Time: 04-Maio-2019 às 04:29
+-- Versão do servidor: 10.1.38-MariaDB
+-- versão do PHP: 7.3.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `votacao`
+-- Database: `banco`
 --
 
 -- --------------------------------------------------------
@@ -26,21 +28,10 @@ SET time_zone = "+00:00";
 -- Estrutura da tabela `cpf`
 --
 
-CREATE TABLE IF NOT EXISTS `cpf` (
+CREATE TABLE `cpf` (
   `cpf` varchar(11) DEFAULT NULL,
-  `codigo` int(11) NOT NULL AUTO_INCREMENT,
-  PRIMARY KEY (`codigo`),
-  UNIQUE KEY `cpf` (`cpf`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
-
---
--- Extraindo dados da tabela `cpf`
---
-
-INSERT INTO `cpf` (`cpf`, `codigo`) VALUES
-('28591383944', 3),
-('46680889850', 1),
-('62726377772', 2);
+  `codigo` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -48,13 +39,12 @@ INSERT INTO `cpf` (`cpf`, `codigo`) VALUES
 -- Estrutura da tabela `ranking`
 --
 
-CREATE TABLE IF NOT EXISTS `ranking` (
+CREATE TABLE `ranking` (
   `votos` int(11) DEFAULT NULL,
   `numero` int(11) NOT NULL,
-  `codigo` int(11) NOT NULL AUTO_INCREMENT,
-  `nome` varchar(60) NOT NULL,
-  PRIMARY KEY (`codigo`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=45 ;
+  `codigo` int(11) NOT NULL,
+  `nome` varchar(60) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Extraindo dados da tabela `ranking`
@@ -66,6 +56,40 @@ INSERT INTO `ranking` (`votos`, `numero`, `codigo`, `nome`) VALUES
 (1, 10, 40, 'JosÃ© Lemes - 10'),
 (1, 0, 43, ''),
 (1, 77, 44, 'Daniel Grandolfo - 77');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `cpf`
+--
+ALTER TABLE `cpf`
+  ADD PRIMARY KEY (`codigo`),
+  ADD UNIQUE KEY `cpf` (`cpf`);
+
+--
+-- Indexes for table `ranking`
+--
+ALTER TABLE `ranking`
+  ADD PRIMARY KEY (`codigo`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `cpf`
+--
+ALTER TABLE `cpf`
+  MODIFY `codigo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `ranking`
+--
+ALTER TABLE `ranking`
+  MODIFY `codigo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=45;
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
